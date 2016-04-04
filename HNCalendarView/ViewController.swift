7c9995ff6407d423b04218708c8209fb7f1b874f
Harry Ng <harryworld@gmail.com>
2016-04-04 23:08:02 +0800

    Initial Commit.

---
+++ HNCalendarView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HNCalendarView
+//
+//  Created by Harry Ng on 4/4/2016.
+//  Copyright © 2016 STAY REAL LIMITED. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
