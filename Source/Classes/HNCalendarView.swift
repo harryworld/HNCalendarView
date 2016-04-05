b2880db140dc17182414051d6002fcc3b9e250a3
Harry Ng <harryworld@gmail.com>
2016-04-05 23:15:36 +0800

    Add main class

---
+++ Source/Classes/HNCalendarView.swift
@@ -0,0 +1,22 @@
+//
+//  HNCalendarView.swift
+//  HNCalendarView
+//
+//  Created by Harry Ng on 5/4/2016.
+//  Copyright © 2016 STAY REAL LIMITED. All rights reserved.
+//
+
+import Cocoa
+
+class HNCalendarView: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    override func viewDidAppear() {
+        
+    }
+    
+}
